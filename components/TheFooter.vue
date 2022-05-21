2e679b2bbcac041ae5269b5badacda64393871c8
cx33 <lne128gene980@163.com>
2022-05-21 23:41:15 +0800

    update: nav

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,10 +3,11 @@
 <template>
   <footer w-full text-center bg-brand-primary text-white flex justify="center" items-center h-40px>
     Copyright &copy;
-    <a href="https://github.com/JiatLn/nuxt3-template" target="_blank">
+    <a href="https://github.com/JiatLn/JiatLn.me" target="_blank">
       JiatLn 2022
     </a>
   </footer>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+</style>
--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,11 +1,43 @@
 <script setup lang="ts">
+import type { RouteLocationRaw } from 'vue-router'
+
+const route = useRoute()
+const currentLink = computed<string>(() => route.name as string)
+
+interface MenuItem {
+  name: string
+  label: string
+  to: RouteLocationRaw
+}
+const menuItems = ref<MenuItem[]>([
+  {
+    label: 'Home',
+    name: 'index',
+    to: { name: 'index' },
+  },
+  {
+    label: 'Article',
+    name: 'article',
+    to: { name: 'article' },
+  },
+  {
+    label: 'About',
+    name: 'about',
+    to: { name: 'about' },
+  },
+])
 </script>
 
 <template>
-  <nav>
-    路由
+  <nav flex="~ c gap-20px" h-40px text="gray-600">
+    <nuxt-link v-for="item in menuItems" :key="item.name" :to="item.to" hover="text-brand-primary" transition="all" :class="{ active: item.name === currentLink }">
+      {{ item.label }}
+    </nuxt-link>
   </nav>
 </template>
 
 <style scoped lang="scss">
+.active {
+  @apply text-brand-primary
+}
 </style>
