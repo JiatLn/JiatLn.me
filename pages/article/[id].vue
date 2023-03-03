6c079f88f3c9c95b4945162c969309a32ec1c1e0
林佳涛 <lne128gene980@163.com>
2023-03-03 15:10:31 +0800

    feat: add blog link to issue

--- pages/article/[id].vue
+++ pages/article/[id].vue
@@ -7,7 +7,7 @@ const route = useRoute()
 const router = useRouter()
 
 const issueId = computed<number>(() => +route.params.id)
-const { issueDetail: article, loading } = useIssueDetail(issueId)
+const { issueDetail: blog, loading } = useIssueDetail(issueId)
 
 const toArticleList = () => {
   router.push({
@@ -33,28 +33,33 @@ const markRender = (content: string) => {
   return result
 }
 
+function linkClick() {
+  window.open(blog.value?.html_url, '_blank')
+}
+
 useHead({
-  title: () => `${article.value?.title || 'Blog'} - JiatLn`,
+  title: () => `${blog.value?.title || 'Blog'} - JiatLn`,
 })
 </script>
 
 <template>
   <div w="80%" lt-sm="w-90%" max-w-900px flex="~ col" mx-auto py-10px>
-    <div v-if="article" mb-60px>
-      <h2 text="center 3xl" my-20px lt-sm="text-xl my-10px">
-        {{ article.title }}
+    <div v-if="blog" mb-60px>
+      <h2 text="center 3xl" my-20px lt-sm="text-xl my-10px" flex="c">
+        <span>{{ blog.title }}</span>
+        <div text="20px" alink ml-10px cp i-carbon-link @click="linkClick" title="to github issue" />
       </h2>
       <div flex="c" gap-40px mb-10px>
         <div flex="c" gap-10px>
           <div text="sm brand-primary" class="i-ant-design:user-outlined" />
-          <span>{{ article.user.login }}</span>
+          <span>{{ blog.user.login }}</span>
         </div>
         <div flex="c" gap-10px>
           <div text="sm brand-primary" class="i-clarity:date-line" />
-          <span>{{ article.created_at.slice(0, 10) }}</span>
+          <span>{{ blog.created_at.slice(0, 10) }}</span>
         </div>
       </div>
-      <article class="markdown-body" p-3 mb-6 min-h-60vh v-html="markRender(article.body)" />
+      <article class="markdown-body" p-3 mb-6 min-h-60vh v-html="markRender(blog.body)" />
       <div flex-c>
         <button self-center flex-c gap-8px alink @click="toArticleList">
           <div i-fluent:arrow-step-back-16-filled />
