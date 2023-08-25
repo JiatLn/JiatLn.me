70e345b665c5019515201c106ca3dde76d9cb9ac
cx33 <lne128gene980@163.com>
2023-08-25 17:34:03 +0800

    update: footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,15 +1,9 @@
-<script setup lang="ts"></script>
-
 <template>
-  <footer z--1 w-full text-white dark:op-70 flex="~ gap-12px" px-20px items-center h-50px lg="fixed bottom-0">
-    <span text="14px">
-      &copy; JiatLn 2023
-    </span>
-    <NuxtLink w="20px" h="20px" href="https://github.com/JiatLn/jiatln.me" target="_blank" hover="op80">
-      <div w-full h-full i-carbon:logo-github />
-    </NuxtLink>
+  <footer z--1 w-full text-white dark:op-70 flex="~ gap-12px" px-20px flex-c h-50px lg="fixed bottom-0">
+    <div text="14px dark:white/60" flex items-center gap-3>
+      <NuxtLink alink target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</NuxtLink>
+      <span>2022-PRESENT</span>
+      <NuxtLink alink target="_blank" href="https://github.com/JiatLn/jiatln.me">&copy; JiatLn</NuxtLink>
+    </div>
   </footer>
 </template>
-
-<style scoped lang="scss">
-</style>
